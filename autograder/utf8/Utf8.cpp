9b9db1bcbe5016cca25fd8a6047918db2b345dbb
Abby Troost <jdtroost@umich.edu>
2026-01-23 22:15:54 -0500

    fixed output and solved edgecases found by KuhnStressTest when flipping twice

--- autograder/utf8/Utf8.cpp
+++ autograder/utf8/Utf8.cpp
@@ -7,42 +7,40 @@
 // the character pointed to by p because p may point to a malformed
 // character.
 
-size_t SizeOfUtf8( Unicode c ) {
-  //   U-00000000 - U-0000007F:   0xxxxxxx 7 bits
-  //   U-00000080 - U-000007FF:   110xxxxx 10xxxxxx 11 bits
-  //   U-00000800 - U-0000FFFF:   1110xxxx 10xxxxxx 10xxxxxx 16 bits
-  //   U-00010000 - U-001FFFFF:   11110xxx 10xxxxxx 10xxxxxx 10xxxxxx 21 bits
-  //   U-00200000 - U-03FFFFFF:   111110xx 10xxxxxx 10xxxxxx 10xxxxxx 10xxxxxx 26 bits
-  //   U-04000000 - U-7FFFFFFF:   1111110x 10xxxxxx 10xxxxxx 10xxxxxx 10xxxxxx 10xxxxxx 31 bits
+size_t SizeOfUtf8(Unicode c) {
+    //   U-00000000 - U-0000007F:   0xxxxxxx 7 bits
+    //   U-00000080 - U-000007FF:   110xxxxx 10xxxxxx 11 bits
+    //   U-00000800 - U-0000FFFF:   1110xxxx 10xxxxxx 10xxxxxx 16 bits
+    //   U-00010000 - U-001FFFFF:   11110xxx 10xxxxxx 10xxxxxx 10xxxxxx 21 bits
+    //   U-00200000 - U-03FFFFFF:   111110xx 10xxxxxx 10xxxxxx 10xxxxxx 10xxxxxx 26 bits
+    //   U-04000000 - U-7FFFFFFF:   1111110x 10xxxxxx 10xxxxxx 10xxxxxx 10xxxxxx 10xxxxxx 31 bits
 
-  if (c <= 0x7F) return 1;
-  if (c <= 0x7FF) return 2;
-  if (c <= 0xFFFF) return 3;
-  if (c <= 0x1FFFFF) return 4;
-  if (c <= 0x3FFFFFF) return 5;
-  if (c <= 0x7FFFFFFF) return 6;
-  return 3; // replacement char
+    if (c <= 0x7F) return 1;
+    if (c <= 0x7FF) return 2;
+    if (c <= 0xFFFF) return 3;
+    if (c <= 0x1FFFFF) return 4;
+    if (c <= 0x3FFFFFF) return 5;
+    if (c <= 0x7FFFFFFF) return 6;
+    return 3;  // replacement char
 }
 
 // SizeOfUtf16 tells the number of bytes it will take to encode the
 // specified value as Utf16. Assumes values over 0xffff will be written as
 // surrogates and that values > 0x10ffff will be replaced.
 
-size_t SizeOfUtf16( Unicode c ) {
-  // check for replacement
-  if (c > 0x10ffff || c <= 0xffff) {
-    return 2; // for replacement char or fits
-  }
-  else {
-    return 4; // two 16-bit surrogates
-  }
+size_t SizeOfUtf16(Unicode c) {
+    // check for replacement
+    if (c > 0x10ffff || c <= 0xffff) {
+        return 2;  // for replacement char or fits
+    } else {
+        return 4;  // two 16-bit surrogates
+    }
 }
-
 
 // IndicatedLength looks at the first byte of a Utf8 sequence and
 // determines the expected length.  Return 1 for an invalid first byte.
 
-size_t IndicatedLength(const Utf8 *p) {
+size_t IndicatedLength(const Utf8* p) {
     Utf8 b = *p;
 
     // check for 1 byte
@@ -53,16 +51,16 @@ size_t IndicatedLength(const Utf8 *p) {
     for (size_t len = 2; len <= 6; len++) {
         // shift b right to leave only the leftmost len bits
         Utf8 top_bits = b >> (8 - len - 1);
-        
+
         // create expected pattern: len ones followed by a 0
         Utf8 pattern = ((1 << len) - 1) << 1;
-        
+
         if (top_bits == pattern) {
             return len;
         }
     }
-    
-    return 1; // invalid first byte
+
+    return 1;  // invalid first byte
 }
 
 // Read the next Utf8 character beginning at **pp and bump *pp past
@@ -79,73 +77,78 @@ size_t IndicatedLength(const Utf8 *p) {
 // surrogates), "noncharacters" 0xfdd0 to 0xfdef, and the values 0xfffe
 // and 0xffff should be returned as the replacement character.
 
-Unicode ReadUtf8( const Utf8 **p, const Utf8 *bound) {
-  if (p == nullptr) return 0;
+Unicode ReadUtf8(const Utf8** p, const Utf8* bound) {
+    if (p == nullptr) return 0;
 
-  // cond. 1: *pp points to or past last valid byte
-  if (bound != nullptr && *p >= bound) {
-    return 0;
-  }
-
-  // guess at length
-  size_t len = IndicatedLength(*p);
-
-  // cond. 2: runs past last valid byte
-  if (bound != nullptr && *p + len > bound) {
-    *p = bound;
-    return ReplacementCharacter;
-  }
-
-  Unicode c = 0;
-
-  if (len == 1) {
-    c = (*p)[0] & 0x7f;
-  } else {
-    // get bits from first byte (varies by length)
-    c = (*p)[0] & (0x7f >> len);
-
-    // get 6 bits from each continuation byte
-    for (size_t i = 1; i < len; i++) {
-      // check if is valid continuation byte (starts with 0b10)
-      if (((*p)[i] >> 6) != 0b10) {
-        *p += i + 1;
-        return ReplacementCharacter;
-      }
-      c = (c << 6) | ((*p)[i] & 0b00111111);
+    // cond. 1: *pp points to or past last valid byte
+    if (bound != nullptr && *p >= bound) {
+        return 0;
     }
-  }
 
-  // overlong
-  if (SizeOfUtf8(c) != len) {
+    // guess at length
+    size_t len = IndicatedLength(*p);
+
+    // cond. 2: runs past last valid byte
+    if (bound != nullptr && *p + len > bound) {
+        *p = bound;
+        return ReplacementCharacter;
+    }
+
+    Unicode c = 0;
+
+    if (len == 1) {
+        // check for malformed byte
+        if ((*p)[0] & 0x80) {
+            (*p)++;
+            return ReplacementCharacter;
+        }
+        c = (*p)[0] & 0x7f;
+    } else {
+        // get bits from first byte (varies by length)
+        c = (*p)[0] & (0x7f >> len);
+
+        // get 6 bits from each continuation byte
+        for (size_t i = 1; i < len; i++) {
+            // check if is valid continuation byte (starts with 0b10)
+            if (((*p)[i] >> 6) != 0b10) {
+                *p += i;
+                return ReplacementCharacter;
+            }
+            c = (c << 6) | ((*p)[i] & 0b00111111);
+        }
+    }
+
+    // overlong
+    if (SizeOfUtf8(c) != len) {
+        *p += len;
+        return ReplacementCharacter;
+    }
+
+    // Overlong characters and character values 0xd800 to 0xdfff (UTF-16
+    // surrogates), "noncharacters" 0xfdd0 to 0xfdef, and the values 0xfffe
+    // and 0xffff should be returned as the replacement character.
+
+    if ((c >= 0xd800 && c <= 0xdfff) ||
+        (c >= 0xfdd0 && c <= 0xfdef) ||
+        c == 0xfffe || c == 0xffff) {
+        *p += len;
+        return ReplacementCharacter;
+    }
+
     *p += len;
-    return ReplacementCharacter;
-  }
-
-  // Overlong characters and character values 0xd800 to 0xdfff (UTF-16
-  // surrogates), "noncharacters" 0xfdd0 to 0xfdef, and the values 0xfffe
-  // and 0xffff should be returned as the replacement character.
-
-  if ((c >= 0xd800 && c <= 0xdfff) ||
-    (c >= 0xfdd0 && c <= 0xfdef) ||
-    c == 0xfffe || c == 0xffff) {
-    *p += len;
-    return ReplacementCharacter;
-  }
-
-  *p += len;
-  return c;
+    return c;
 }
 
 // Scan backward for the first PREVIOUS byte which could
 // be the start of a UTF-8 character.  If bound != null,
 // bound points to the first (leftmost) valid byte.
 
-const Utf8 *PreviousUtf8(const Utf8 *p, const Utf8 *bound) {
-  for(;;) {
-    if (bound != nullptr && p <= bound) return p; // hit boundary
-    p--;
-    if ((*p & 0b11000000) != 0b10000000) return p; // if is not continuation
-  }
+const Utf8* PreviousUtf8(const Utf8* p, const Utf8* bound) {
+    for (;;) {
+        if (bound != nullptr && p <= bound) return p;  // hit boundary
+        p--;
+        if ((*p & 0b11000000) != 0b10000000) return p;  // if is not continuation
+    }
 }
 
 // Write a Unicode character in UTF-8, returning one past
@@ -157,31 +160,31 @@ const Utf8 *PreviousUtf8(const Utf8 *p, const Utf8 *bound) {
 //
 // If c > 0x7fffffff (31 bits) write the replacement character.
 
-Utf8 *WriteUtf8(Utf8 *p, Unicode c, Utf8 *bound) {
-  size_t len = SizeOfUtf8(c);
-  if (bound != nullptr && p + len > bound) return p;
+Utf8* WriteUtf8(Utf8* p, Unicode c, Utf8* bound) {
+    size_t len = SizeOfUtf8(c);
+    if (bound != nullptr && p + len > bound) return p;
 
-  if (c > 0x7fffffff) {
-    c = ReplacementCharacter;
-  }
+    if (c > 0x7fffffff) {
+        c = ReplacementCharacter;
+    }
 
-  // write continuation bytes (if any), from back to front
-  for (size_t i = len; i-- > 1; ) {
-    p[i] = static_cast<Utf8>(0b10000000 | (c & 0b00111111));
-    c >>= 6;
-  }
+    // write continuation bytes (if any), from back to front
+    for (size_t i = len; i-- > 1;) {
+        p[i] = static_cast<Utf8>(0b10000000 | (c & 0b00111111));
+        c >>= 6;
+    }
 
-  // leading byte:
-  // for len = 1: mask = 0x00, payload = 7 bits
-  // for len > 1: mask = ((1 << len) - 1) << (8 - len)
-  if (len == 1) {
-    p[0] = static_cast<Utf8>(c & 0x7F);
-  } else {
-    Utf8 lead_mask = static_cast<Utf8>(((1u << len) - 1) << (8 - len));
-    p[0] = static_cast<Utf8>(lead_mask | c);
-  }
+    // leading byte:
+    // for len = 1: mask = 0x00, payload = 7 bits
+    // for len > 1: mask = ((1 << len) - 1) << (8 - len)
+    if (len == 1) {
+        p[0] = static_cast<Utf8>(c & 0x7F);
+    } else {
+        Utf8 lead_mask = static_cast<Utf8>(((1u << len) - 1) << (8 - len));
+        p[0] = static_cast<Utf8>(lead_mask | c);
+    }
 
-  return p + len;
+    return p + len;
 }
 
 // Read a Utf16 little-endian character beginning at **pp and
@@ -193,35 +196,35 @@ Utf8 *WriteUtf8(Utf8 *p, Unicode c, Utf8 *bound) {
 // a low surrogate. Unpaired or out-of-order surrogates are
 // read as literal values.
 
-Unicode ReadUtf16( const Utf16 **p, const Utf16 *bound ) {
-  Utf16 first = **p;
+Unicode ReadUtf16(const Utf16** p, const Utf16* bound) {
+    Utf16 first = **p;
 
-  if (bound != nullptr && *p >= bound) return 0;
-  
-  (*p)++;
+    if (bound != nullptr && *p >= bound) return 0;
 
-  // check for high surrogate
-  if (first >= 0xd800 && first <= 0xdbff) {
-    if (bound != nullptr && *p >= bound) {
-        return first; // unpaired due to bound, return literal
+    (*p)++;
+
+    // check for high surrogate
+    if (first >= 0xd800 && first <= 0xdbff) {
+        if (bound != nullptr && *p >= bound) {
+            return first;  // unpaired due to bound, return literal
+        }
+
+        Utf16 second = **p;
+
+        // check for low surrogate
+        if (second >= 0xdc00 && second <= 0xdfff) {
+            (*p)++;
+
+            Unicode c = 0x10000 + ((first & 0b1111111111) << 10) + (second & 0b1111111111);
+            return c;
+        }
+
+        // out-of-order or unpaired
+        return first;
     }
 
-    Utf16 second = **p;
-
-    // check for low surrogate
-    if (second >= 0xdc00 && second <= 0xdfff) {
-      (*p)++;
-
-      Unicode c = 0x10000 + ((first & 0b1111111111) << 10) + (second & 0b1111111111);
-      return c;
-    }
-
-    // out-of-order or unpaired
+    // not a surrogate, return as-is
     return first;
-  }
-
-  // not a surrogate, return as-is
-  return first;
 }
 
 // Utf16 uses aligned 16-bit characters in either big-endian (high byte first)
@@ -241,7 +244,6 @@ Unicode ReadUtf16( const Utf16 **p, const Utf16 *bound ) {
 //
 // Reading consists of reversing the steps.
 
-
 // Write a Unicode character in UTF-16, returning one past
 // the the last byte that was written.
 //
@@ -251,61 +253,58 @@ Unicode ReadUtf16( const Utf16 **p, const Utf16 *bound ) {
 //
 // If c > 0x10ffff (21 bits) write the replacement character.
 
-Utf16 *WriteUtf16( Utf16 *p, Unicode c, Utf16 *bound ) {
-  size_t needed = SizeOfUtf16(c) / 2;
+Utf16* WriteUtf16(Utf16* p, Unicode c, Utf16* bound) {
+    size_t needed = SizeOfUtf16(c) / 2;
 
-  // bound check
-  if (bound != nullptr && p + needed > bound) return p;
+    // bound check
+    if (bound != nullptr && p + needed > bound) return p;
 
-  if (c <= 0xffff) {
-    // literal
-    *p++ = c;
-  }
-  else if (c > 0x10ffff) {
-    // replacement
-    *p++ = ReplacementCharacter;
-  }
-  else {
-    // needs surrogate
+    if (c <= 0xffff) {
+        // literal
+        *p++ = c;
+    } else if (c > 0x10ffff) {
+        // replacement
+        *p++ = ReplacementCharacter;
+    } else {
+        // needs surrogate
 
-    // 1.  0x10000 is subtracted from the Unicode value.
-    // 2.  The High Surrogate is written first as 0xd800 + high 10 bits.
-    // 5.  The Low Surrogate is written second as 0xdc00 + low 10 bits.
-    // 6.  Unpaired / out-of-ordersurrogates are read as their literal 16-bit values.
+        // 1.  0x10000 is subtracted from the Unicode value.
+        // 2.  The High Surrogate is written first as 0xd800 + high 10 bits.
+        // 5.  The Low Surrogate is written second as 0xdc00 + low 10 bits.
+        // 6.  Unpaired / out-of-ordersurrogates are read as their literal 16-bit values.
 
-    c -= 0x10000;
-    Utf16 high = 0xd800 + ((c >> 10) & 0b1111111111);
-    Utf16 low = 0xdc00 + (c & 0b1111111111);
+        c -= 0x10000;
+        Utf16 high = 0xd800 + ((c >> 10) & 0b1111111111);
+        Utf16 low = 0xdc00 + (c & 0b1111111111);
 
-    *p++ = high;
-    *p++ = low;
-  }
+        *p++ = high;
+        *p++ = low;
+    }
 
-  return p;
+    return p;
 }
 
 // Wrappers that read the character but don't advance the pointer.
 
-Unicode GetUtf8( const Utf8 *p, const Utf8 *bound ) {
-  // copy the pointer
-  const Utf8* temp = p;
-  return ReadUtf8(&temp, bound);
+Unicode GetUtf8(const Utf8* p, const Utf8* bound) {
+    // copy the pointer
+    const Utf8* temp = p;
+    return ReadUtf8(&temp, bound);
 }
 
-Unicode GetUtf16( const Utf16 *p, const Utf16 *bound ) {
-  const Utf16* temp = p;
-  return ReadUtf16(&temp, bound);
+Unicode GetUtf16(const Utf16* p, const Utf16* bound) {
+    const Utf16* temp = p;
+    return ReadUtf16(&temp, bound);
 }
-
 
 // Wrappers that advance the pointer but throw away the value.
 
-const Utf8 *NextUtf8( const Utf8 *p, const Utf8 *bound ) {
-  ReadUtf8(&p, bound);
-  return p;
+const Utf8* NextUtf8(const Utf8* p, const Utf8* bound) {
+    ReadUtf8(&p, bound);
+    return p;
 }
 
-const Utf16 *NextUtf16( const Utf16 *p, const Utf16 *bound ) {
-  ReadUtf16(&p, bound);
-  return p;
+const Utf16* NextUtf16(const Utf16* p, const Utf16* bound) {
+    ReadUtf16(&p, bound);
+    return p;
 }
--- autograder/utf8/flip.cpp
+++ autograder/utf8/flip.cpp
@@ -1,117 +1,118 @@
-#include "Utf8.h"
-#include <iostream>
 #include <fstream>
+#include <iostream>
 
-// check if c is replacement char before returning
-void Utf8ToUtf16(const Utf8 **p, const Utf8 *bound) {
-  Utf16 buffer[2];
+#include "Utf8.h"
 
-  std::cout << ByteOrderMark;
+void Utf16BigEndianToLittleEndian(Utf16* p, const size_t length) {
+    for (size_t i = 0; i < length; i++) {
+        *p = ((*p & 0xFF) << 8) | ((*p & 0xFF00) >> 8);
 
-  for(;;) {
-    Unicode c = ReadUtf8(p, bound);
-
-    if (*p >= bound && !c) return;
-
-    size_t size = SizeOfUtf16(c);
-    Utf16* end = WriteUtf16(buffer, c);
-
-    std::cout << buffer[0];
-
-    if (size > 2) {
-      std::cout << buffer[1];
+        p++;
     }
-  }
 }
 
-void Utf16ToUtf8(const Utf16 **p, const Utf16 *bound, bool big_endian) {
-  Utf8 buffer[6];
+void Utf8ToUtf16(const Utf8** p, const Utf8* bound) {
+    Utf16 buffer[2];
 
-  for(;;) {
-    Unicode c = ReadUtf16(p, bound);
+    fwrite((uint16_t*)&ByteOrderMark, sizeof(uint16_t), 1, stdout);
 
-    if (*p >= bound) return;
+    for (;;) {
+        const Utf8* p_prev = *p;
+        Unicode c = ReadUtf8(p, bound);
 
-    size_t size = SizeOfUtf8(c);
-    Utf8* end = WriteUtf8(buffer, c);
+        if ((*p >= bound && !c) || (*p == p_prev)) return;
 
-    for (size_t i = 0; i < size; i++) {
-      std::cout << buffer[i];
+        size_t size = SizeOfUtf16(c);
+        Utf16* end = WriteUtf16(buffer, c);
+
+        fwrite(buffer, sizeof(Utf16), 1 + (size > 2), stdout);
     }
-  }
 }
 
-int main(int argc, char *argv[]) {
-  std::ios::sync_with_stdio(false);
-  std::cin.tie(nullptr);
+void Utf16ToUtf8(const Utf16** p, const Utf16* bound) {
+    Utf8 buffer[6];
 
-  if (argc < 2) {
-    std::cout << "Usage: flip <filename>\n";
-    std::cout << "Recognize the input file as either Utf8 or Utf16, convert\n";
-    std::cout << "it to the opposite encoding, and write the result to stdout.\n";
+    (*p)++;  // skip BOM
 
-    return 1;
-  }
+    for (;;) {
+        const Utf16* p_prev = *p;
+        Unicode c = ReadUtf16(p, bound);
 
-  // open fstream
-  std::ifstream Input;
-  Input.open(argv[1], std::ios::binary|std::ios::ate);
+        if (*p == p_prev) return;
 
-  if (Input.fail()) {
-    std::cout << argv[1] << " not found.\n";
-    return 1;
-  }
+        size_t size = SizeOfUtf8(c);
+        Utf8* end = WriteUtf8(buffer, c);
 
-  // recommend reading the whole file into memory as binary data to start
-  // Source - https://stackoverflow.com/a
-  // Posted by David Haim, modified by community. See post 'Timeline' for change history
-
-  // Retrieved 2026-01-23, License - CC BY-SA 3.0
-
-  auto fileSize = Input.tellg();
-  Input.seekg(std::ios::beg);
-  std::string content(fileSize,0);
-  Input.read(&content[0],fileSize);
-
-  // check for type
-  bool big_endian = false;
-  bool is_utf8 = false;
-
-  if (content.size() >= 2) { // enough space for a bom?
-    char first = content[0];
-    char second = content[1];
-
-    // turn it into unicode
-    Unicode read_bom = ((first << 8) | second);
-    if (read_bom == ByteOrderMark) {
-      // Utf16
-      // do nothing
+        fwrite(buffer, sizeof(Utf8), size, stdout);
     }
-    else if (read_bom == BigEndianBOM) {
-      // bigend Utf16
-      big_endian = true;
-    }
-    else {
-      is_utf8 = true;
-    }
-  }
-  else {
-    is_utf8 = true;
-  }
-
-  // time to actually do stuff
-  if (is_utf8) {
-    const Utf8* p = reinterpret_cast<const Utf8*>(content.data());
-    const Utf8* end = p + content.size();
-
-    Utf8ToUtf16(&p, end);
-  }
-  else {
-    const Utf16* p = reinterpret_cast<const Utf16*>(content.data());
-    const Utf16* end = p + content.size() / sizeof(Utf16);
-
-    Utf16ToUtf8(&p, end, big_endian);
-  }
 }
 
+int main(int argc, char* argv[]) {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
 
+    if (argc < 2) {
+        std::cout << "Usage: flip <filename>\n";
+        std::cout << "Recognize the input file as either Utf8 or Utf16, convert\n";
+        std::cout << "it to the opposite encoding, and write the result to stdout.\n";
+
+        return 1;
+    }
+
+    // open fstream
+    std::ifstream Input;
+    Input.open(argv[1], std::ios::binary | std::ios::ate);
+
+    if (Input.fail()) {
+        std::cout << argv[1] << " not found.\n";
+        return 1;
+    }
+
+    // recommend reading the whole file into memory as binary data to start
+    // Source - https://stackoverflow.com/a
+    // Posted by David Haim, modified by community. See post 'Timeline' for change history
+
+    // Retrieved 2026-01-23, License - CC BY-SA 3.0
+
+    auto fileSize = Input.tellg();
+    Input.seekg(std::ios::beg);
+    std::string content(fileSize, 0);
+    Input.read(&content[0], fileSize);
+
+    // check for type
+    bool big_endian = false;
+    bool is_utf8 = false;
+
+    if (content.size() >= 2) {  // enough space for a bom?
+        char first = content[0];
+        char second = content[1];
+
+        // turn it into unicode
+        Unicode read_bom = (((Unicode)second & 0xFF) << 8) | ((Unicode)first & 0xFF);
+        if (read_bom == ByteOrderMark) {
+            // Utf16
+            // do nothing
+        } else if (read_bom == BigEndianBOM) {
+            // bigend Utf16
+            big_endian = true;
+        } else {
+            is_utf8 = true;
+        }
+    } else {
+        is_utf8 = true;
+    }
+
+    // time to actually do stuff
+    if (is_utf8) {
+        const Utf8* p = reinterpret_cast<const Utf8*>(content.data());
+        const Utf8* end = p + content.size();
+
+        Utf8ToUtf16(&p, end);
+    } else {
+        const Utf16* p = reinterpret_cast<const Utf16*>(content.data());
+        const Utf16* end = p + content.size() / sizeof(Utf16);
+
+        if (big_endian) Utf16BigEndianToLittleEndian(reinterpret_cast<Utf16*>(content.data()), content.size() / sizeof(Utf16));
+        Utf16ToUtf8(&p, end);
+    }
+}
