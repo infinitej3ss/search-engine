f3f253c6e98b7e9de3634c84d150e748c6393829
lytreallynb <yutonglv@umich.edu>
2026-01-29 22:54:13 -0500

    uodate hashtable.h

--- autograder/hashtable/TopN.cpp
+++ autograder/hashtable/TopN.cpp
@@ -27,51 +27,47 @@ Pair **TopN( Hash *hashtable, int N )
 
    // YOUR CODE HERE
     
-    // need a comparator, greater
-    // bool ( *greater ) ( const Value, const Value);
     
         // Create the dynamic allocated array, initialized with nullptr
-        Pair** result = new Pair[N];
+        Pair** result = new Pair*[N];
         for (int i = 0; i < N; ++i) {
             result[i] = nullptr;
         }
         int count = 0;
     
         // Go through the HashTable and insert all elements in the TopN array
-        for (size_t i = 0; i < hashtable->numberOfBuckets; ++i) {
-            Bucket b = hashtable->buckets[i];
-            while (b != nullptr) {
-                if (count < N) {
-                    int j = count;
-                    // directly insert the ptr of tuple
-                    result[j] = b->tuple;
-                    count++;
-                    // should sort every time
-                    while (j > 0 && greater(result[j]->Value, result[j - 1]->Value)) {
-                        // swap (avoid STL)
+        for (auto it = hashtable->begin(); it != hashtable->end(); ++it) {
+            
+            Pair* cur = &(*it);
+            
+            if (count < N) {
+                int j = count;
+                // directly insert the ptr of tuple
+                result[j] = cur;
+                count++;
+                // should sort every time
+                while (j > 0 && result[j]->value > result[j - 1]->value) {
+                    // swap (avoid STL)
+                    Pair* tmp = result[j - 1];
+                    result[j - 1] = result[j];
+                    result[j] = tmp;
+                    j--;
+                }
+            } else {
+                // do comparision of the values
+                // if greater, push_back at the end the array, then shifting
+                // elements to keep the array sorted in descending order, to
+                // avoid unnecessary shiftings
+                int j = N - 1;
+                if (cur->value > result[j]->value) {
+                    result[j] = cur;
+                    while (j > 0 && result[j]->value > result[j - 1]->value) {
                         Pair* tmp = result[j - 1];
                         result[j - 1] = result[j];
                         result[j] = tmp;
                         j--;
                     }
-                } else {
-                    // do comparision of the values
-                    // if greater, push_back at the end the array, then shifting
-                    // elements to keep the array sorted in descending order, to
-                    // avoid unnecessary shiftings
-                    int j = N - 1;
-                    if (greater(b->tuple->Value, result[j]->Value)) {
-                        result[j] = b->tuple;
-                        while (j > 0 && greater(result[j]->Value, result[j - 1]->Value)) {
-                            Pair* tmp = result[j - 1];
-                            result[j - 1] = result[j];
-                            result[j] = tmp;
-                            j--;
-                        }
-                        
-                    }
                 }
-                b = b->next;
             }
         }
     
