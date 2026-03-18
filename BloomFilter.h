8fe03fc3bfcc6670996b2d293679f37be42b2980
Abby Troost <jdtroost@umich.edu>
2026-03-18 17:56:16 -0400

    implemented insert, contains, and hash

--- BloomFilter.h
+++ BloomFilter.h
@@ -1,12 +1,14 @@
 #ifndef BLOOMFILTER_H
 #define BLOOMFILTER_H
 
-#include <vector>
+#include <openssl/evp.h>
+#include <openssl/md5.h>
+#include <string.h>
+
 #include <bitset>
 #include <cmath>
 #include <string>
-#include <string.h>
-#include <openssl/md5.h>
+#include <vector>
 
 class Bloomfilter {
     public:
@@ -26,7 +28,10 @@ class Bloomfilter {
             std::pair<uint64_t, uint64_t> hashPair = hash(s);
 
             // Use double hashing to get unique bit, and repeat for each hash function.
-
+            for(int i = 0; i < numHashes; i++) {
+                int filter_index = (hashPair.first + (i * hashPair.second)) % filterSize; 
+                filter[filter_index] = true;
+            }
         }   
 
         bool contains( const std::string &s ) {
@@ -35,12 +40,13 @@ class Bloomfilter {
         
             // Use double hashing to get unique bit, and repeat for each hash function.
 
-            // If bit is false, we know for certain this unique string has not been inserted.
-
-            // If all bits were true, the string is likely inserted, but false positive is possible.
-
-            // This line is for compiling, replace this with own code.
-            return false;
+            for (int i = 0; i < numHashes; i++) {
+                int filter_index = (hashPair.first + (i * hashPair.second)) % filterSize;
+                if(!filter[filter_index]) {
+                    return false;  // If bit is false, we know for certain this unique string has not been inserted.
+                }
+            }
+            return true;  // If all bits were true, the string is likely inserted, but false positive is possible.
         }   
 
         private:
@@ -53,8 +59,10 @@ class Bloomfilter {
         std::pair<uint64_t, uint64_t> hash( const std::string &datum ) {
             //Use MD5 to hash the string into one 128 bit hash, and split into 2 hashes.
             
-            //This line is for compiling, replace this with own code.
-            return {0,0};
+            uint64_t full_hash_value[2];
+            //*MD5((unsigned char*)datum.c_str(), datum.length(), (unsigned char*)full_hash_value); // depreciated
+            EVP_Q_digest(NULL, "MD5", NULL, datum.c_str(), datum.size(), (unsigned char*)full_hash_value, NULL);
+            return {full_hash_value[0], full_hash_value[1]};
         }
     };
 
