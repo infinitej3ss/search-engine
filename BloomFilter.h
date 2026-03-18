1785e0e33b0422f94bb0e2a8566c120bde0f26b8
Abby Troost <jdtroost@umich.edu>
2026-03-18 18:59:51 -0400

    sanity checking for filter size

--- BloomFilter.h
+++ BloomFilter.h
@@ -15,7 +15,13 @@ class Bloomfilter {
         Bloomfilter( int num_objects, double false_positive_rate ) {
             // Determine the size of bits of our data vector, and resize.
             filterSize = int(-1 * (num_objects * std::log(false_positive_rate)) / (std::log(2) * std::log(2)));
-            
+
+            // sanity checking for size of filter
+            size_t max_size = (filter.max_size() < 100000000) ? filter.max_size() / 2 : 100000000;
+            if (filterSize > max_size) {
+                filterSize = max_size;
+            }
+
             // Resize filter accordingly
             filter.resize(filterSize);
 
@@ -53,15 +59,15 @@ class Bloomfilter {
         // Add any private member variables that may be neccessary.
 
         std::vector<bool> filter;
-        int filterSize;
+        size_t filterSize;
         int numHashes;
 
         std::pair<uint64_t, uint64_t> hash( const std::string &datum ) {
             //Use MD5 to hash the string into one 128 bit hash, and split into 2 hashes.
             
             uint64_t full_hash_value[2];
-            //*MD5((unsigned char*)datum.c_str(), datum.length(), (unsigned char*)full_hash_value); // depreciated
-            EVP_Q_digest(NULL, "MD5", NULL, datum.c_str(), datum.size(), (unsigned char*)full_hash_value, NULL);
+            MD5((unsigned char*)datum.c_str(), datum.length(), (unsigned char*)full_hash_value); // depreciated but openssl version on autograder is old enough for it to be fine ig
+            //EVP_Q_digest(NULL, "MD5", NULL, datum.c_str(), datum.size(), (unsigned char*)full_hash_value, NULL);
             return {full_hash_value[0], full_hash_value[1]};
         }
     };
