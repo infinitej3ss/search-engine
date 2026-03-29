c160e34a9cc4c87cd5478f9bc5fafe380d15e88a
Jess Morton <infinitejess03@gmail.com>
2026-03-29 10:17:42 -0400

    Finish stubs

--- crawler/get_ssl.cpp
+++ crawler/get_ssl.cpp
@@ -5,10 +5,15 @@ enum get_url_return {
     failure,
     blacklist,
     success
-}
+};
 
 
 // take a URL and return the HTML
-get_url_return get_ssl(){
+get_url_return get_ssl(std::string &page){
 
+    // TODO: implement functionality
+
+    page = "<!DOCTYPE html><html><head></head><body>Hello world!</body></html>";
+
+    return success;
 }
