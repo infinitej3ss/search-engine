d3102d820ea5a882c337a2ea848e27e231f5a039
Abby Troost <jdtroost@umich.edu>
2026-04-05 18:05:10 -0400

    testing framework

---
+++ tests/test_page_file_initialization.cpp
@@ -0,0 +1,21 @@
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_floating_point.hpp>
+
+#include "crawler/page_data.h"
+#include "crawler/page_data.cpp"
+
+using Catch::Matchers::WithinAbs;
+
+TEST_CASE("initialize dirs for writing", "[crawler]") {
+    int result = initialize_page_file_dir("../crawler_test_files/");
+    REQUIRE(result != -1);
+
+    REQUIRE(PAGE_FILES[0].num_files_of_this_rank_written == 1);
+}
+
+TEST_CASE("initialize dirs for reading", "[crawler]") {
+    std::vector<std::vector<std::string>> result = get_page_file_names("../crawler_test_files/");
+    REQUIRE(result[0].size() > 0);
+
+    REQUIRE(result[0][0] == "../crawler_test_files/crawled_page_data_rank_0_num_0");
+}
