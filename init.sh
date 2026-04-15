1a345e7bb5efb682ec61d32a450b81c28090ba9d
Jess Morton <infinitejess03@gmail.com>
2026-04-15 02:16:34 -0400

    Finally finalize init.sh

--- init.sh
+++ init.sh
@@ -6,6 +6,10 @@ rm -rf crawler_test_files/*dir # remove any pre existing directories
 
 mkdir -p crawler_test_files/page_data_dir crawler_test_files/frontier_dir crawler_test_files/bloom_filter_dir
 
+rm -rf build
+
+cmake -S . -B build
+
 cmake --build build
 
 ./build/crawler src/init.config crawler_test_files/seedlist.txt crawler_test_files/page_data_dir/ crawler_test_files/frontier_dir/ crawler_test_files/bloom_filter_dir/ 200 "$1"
