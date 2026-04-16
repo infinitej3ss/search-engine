98af3fbfcb539ebf44cd5e0a1b2b0bd53fffdce4
Jess Morton <infinitejess03@gmail.com>
2026-04-16 13:23:12 -0400

    Add rebuid script

---
+++ rebuild_and_continue.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+rm -rf build
+
+cmake -S . -B build
+
+cmake --build build
+
+./build/crawler src/init.config crawler_test_files/seedlist.txt crawler_test_files/page_data_dir/ crawler_test_files/frontier_dir/ crawler_test_files/bloom_filter_dir/ 500 "$1" 1
